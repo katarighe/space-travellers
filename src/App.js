47dc1f0e281e068203f55bb9c31a692940559ea0
MargaretTechworld <margarettechworld@gmail.com>
2023-08-28 13:13:44 +0000

    add routes to dragon component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Rockets from './components/Rockets';
+import Dragons from './components/Dragons';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Rockets />} />
+        <Route exact path="/dragons" element={<Dragons />} />
       </Routes>
     </>
   );
