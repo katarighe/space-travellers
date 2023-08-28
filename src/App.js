c9315d6189ad24315e10f57007941407b73c544f
katarighe <m.ighe@hotmail.com>
2023-08-28 18:45:22 +0300

    Add Missions onto the App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Rockets from './components/Rockets';
+import Missions from './components/Missions';
 import Dragons from './components/Dragons';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Rockets />} />
+        <Route exact path="/Missions" element={<Missions />} />
         <Route exact path="/Dragons" element={<Dragons />} />
       </Routes>
     </>
