bc8970bf3084df814220b65de4f1ce9d35d18326
katarighe <m.ighe@hotmail.com>
2023-08-28 16:20:29 +0300

    Create Rockets slice

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,1 +1,30 @@
-// eslint-disable-file no-use-before-define
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const ALL_ROCKETS = 'space travelers/redux/rockets/ALL_ROCKETS';
+
+export const rocketsReducer = (state = [], action) => {
+  switch (action.type) {
+    case `${ALL_ROCKETS}/fulfilled`:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export const fetchRockets = createAsyncThunk(ALL_ROCKETS, async () => {
+  const data = [];
+  await fetch('')
+    .then((res) => res.json())
+    .then((json) => {
+      json.forEach((i) => {
+        data.push({
+          id: i.id,
+          name: i.rocket_name,
+          type: i.rocket_type,
+          flickr_images: i.flickr_images,
+          description: i.description,
+        });
+      });
+    });
+  return data;
+});
