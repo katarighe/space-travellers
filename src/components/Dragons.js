eadcb7ff10807d99090d7e32aa5d5c55b755051f
katarighe <m.ighe@hotmail.com>
2023-08-28 18:16:29 +0300

    Re-add from my partner Margaret Kojo-Musa

--- src/components/Dragons.js
+++ src/components/Dragons.js
@@ -1,1 +1,39 @@
-// eslint-disable-file no-use-before-define
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchDragons } from '../redux/dragons/dragons';
+
+const Dragons = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchDragons());
+  }, [dispatch]);
+
+  const allDragons = useSelector((state) => state.dragons);
+
+  return (
+    <ul className="container">
+      {allDragons.map((dragon) => (
+        <li key={dragon.id} className="dragon_id">
+          <div className="dragon">
+            <img
+              src={dragon.flickr_images[0]}
+              alt="dragon"
+              style={{ width: '250px', height: '200px' }}
+              className="p-0"
+            />
+          </div>
+          <div>
+            <h2 style={{ fontSize: '1.5em' }}>{dragon.name}</h2>
+            <p>{dragon.description}</p>
+            <button type="button" className="btn btn-primary">
+              Reserve Dragon
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Dragons;
