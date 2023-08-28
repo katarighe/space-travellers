c14c23a902c0c0d91cfae833ff0fa18154629ea3
katarighe <m.ighe@hotmail.com>
2023-08-28 13:32:11 +0300

    Fix linter errors

--- src/components/MissionList.js
+++ src/components/MissionList.js
@@ -1,13 +1,1 @@
-// eslint-disable-file no-use-before-define 
-function App() {
-    return (
-      <h>
-        Write your statement here
-      </h>
-    );
-  }
-  
-  test('App renders the correct text', () => {
-    const { getByText } = render(<App />);
-    expect(getByText('Write your statement here')).toBeTruthy();
-  });
+// eslint-disable-file no-use-before-define
