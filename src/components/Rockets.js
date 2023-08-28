032cdf5e4a52122c0a7c3dfbf0839b419421fdff
katarighe <m.ighe@hotmail.com>
2023-08-28 15:53:20 +0300

    Rewrite the functions for the Rockets page

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,1 +1,39 @@
-// eslint-disable-file no-use-before-define
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../redux/rockets/rockets';
+
+const Rockets = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
+  const allRockets = useSelector((state) => state.rockets);
+
+  return (
+    <ul className="container">
+      {allRockets.map((rocket) => (
+        <li key={rocket.id} className="rocket_id">
+          <div className="rocket">
+            <img
+              src={rocket.flickr_images[0]}
+              alt="rocket"
+              style={{ width: "250px", height: "200px" }}
+              className="p-0"
+            />
+          </div>
+          <div>
+            <h2 style={{ fontSize: "1.5em" }}>{rocket.name}</h2>
+            <p>{rocket.description}</p>
+            <button type="button" className="btn btn-primary">
+              Reserve Rocket
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Rockets;
