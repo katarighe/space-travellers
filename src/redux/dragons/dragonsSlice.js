5ca3b0a5dd6134c05fc17dd5256abc6259b82aca
MargaretTechworld <margarettechworld@gmail.com>
2023-08-29 16:27:10 +0000

    fetch dragon data

--- src/redux/dragons/dragonsSlice.js
+++ src/redux/dragons/dragonsSlice.js
@@ -1,28 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  dragons: {},
+  dragons: [],
   loading: false,
   error: null,
 };
 
+export const getDragons = createAsyncThunk('dragons/getDragons', async () => {
+  const dragons = await fetch('https://api.spacexdata.com/v3/dragons');
+  const data = await dragons.json();
+  return data;
+});
 const dragonsSlice = createSlice({
   name: 'dragons',
   initialState,
-  reducers: {
-    // This reducer will be called when the `missions` action is dispatched
-    setDragons(state, action) {
-      state.dragons = action.payload;
-    },
-    // This reducer will be called when the `loading` action is dispatched
-    setLoading(state, action) {
-      state.loading = action.payload;
-    },
-    // This reducer will be called when the `error` action is dispatched
-    setError(state, action) {
-      state.error = action.payload;
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getDragons.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getDragons.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = '';
+        state.dragons = action.payload;
+      })
+      .addCase(getDragons.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
   },
+
 });
 
 export default dragonsSlice.reducer;
