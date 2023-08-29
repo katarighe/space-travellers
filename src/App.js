aae70d13993eb85a6d0b4328a5afd64c56251bd3
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-08-29 11:47:44 +0330

    Add profile page to routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 import Dragons from './components/Dragons';
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Rockets />} />
-        <Route exact path="/Missions" element={<Missions />} />
-        <Route exact path="/Dragons" element={<Dragons />} />
+        <Route exact path="/missions" element={<Missions />} />
+        <Route exact path="/dragons" element={<Dragons />} />
+        <Route exact path="/profile" element={<Profile />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
 
