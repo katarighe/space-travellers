c2bcfe879fc85294080eb96c210bc5760a495b1b
katarighe <m.ighe@hotmail.com>
2023-08-29 13:43:59 +0300

    Fix the reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { logger } from 'redux-logger';
-import Rockets from './rockets/rocketsSlice';
+import rocketsReducer from './rockets/rocketsSlice';
 import missionsReducer from './missions/missionsSlice';
 import dragonsReducer from './dragons/dragonsSlice';
 
 const store = configureStore({
   reducer: {
-    rockets: Rockets.reducer,
+    rockets: rocketsReducer,
     missions: missionsReducer,
     dragons: dragonsReducer,
   },
