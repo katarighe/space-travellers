8f04ebdef94db6596cf7e195174bc147657cc4fe
katarighe <m.ighe@hotmail.com>
2023-08-29 20:33:37 +0300

    Fix the styles

--- src/components/Dragons.js
+++ src/components/Dragons.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getDragons } from '../redux/dragons/dragonsSlice';
-import '../dragon.css';
+import styles from './';
 
 const Dragons = () => {
   const dispatch = useDispatch();
@@ -13,18 +13,18 @@ const Dragons = () => {
   const { loading, error, dragons } = useSelector((state) => state.dragons);
 
   return (
-    <section className="dragon-container">
+    <section className={styles.dragon_container}>
       {loading && <p>Dragons loading, please wait!</p>}
       {error && <p>Error loading dragons, please try again!</p>}
       {dragons.map((dragon) => (
-        <div key={dragon.id} className="dragon-id">
-          <div className="dragon-info">
-            <img src={dragon.flickr_images[0]} alt="dragon" className="dragon-img" />
+        <div key={dragon.id} className={styles.dragon_id}>
+          <div className={styles.dragon_info}>
+            <img src={dragon.flickr_images[0]} alt="dragon" className={styles.dragon_img} />
           </div>
-          <div className="d-name-type">
-            <h2 className="dragon-name">{dragon.name}</h2>
-            <p className="dragon-type">{dragon.type}</p>
-            <button type="button" className="dragon-button">
+          <div className={styles.d_name_type}>
+            <h2 className={styles.dragon_name}>{dragon.name}</h2>
+            <p className={styles.dragon_type}>{dragon.type}</p>
+            <button type="button" className={styles.dragon_button}>
               Reserve Dragon
             </button>
           </div>
