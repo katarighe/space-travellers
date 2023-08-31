989612b6b60fc60f390f59afedc98fcc3e0111f5
MargaretTechworld <margarettechworld@gmail.com>
2023-08-31 08:02:58 +0000

    Update Dragon component to retain rendered dragon data when user switch to another page in the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 import Dragons from './components/Dragons';
 import Profile from './components/Profile';
+import { getDragons } from './redux/dragons/dragonsSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getDragons());
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <Navbar />
